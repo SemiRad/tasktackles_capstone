e7390f57807ca6ae52ff68d27548010e2cc67328
SemiRaden <radenc3@gmail.com>
2023-10-13 20:05:44 +0800

    migration

--- database/migrations/2023_10_13_120135_create_messages_table.php
+++ database/migrations/2023_10_13_120135_create_messages_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('message', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_sender_id')->constrained('user');
+            $table->foreignId('user_receiver_id')->constrained('user');
+            $table->string('text');
+            $table->timestamp('timestamp');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('messages');
     }
 };
