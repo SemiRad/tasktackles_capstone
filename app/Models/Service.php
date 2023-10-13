a570b470fe94b2889f7cc5a6a765b6c1c1e14f3c
SemiRaden <radenc3@gmail.com>
2023-10-13 20:07:02 +0800

    las vegas models

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    protected $table = 'service';
+    protected $fillable = [
+        'service_list_name',
+        'user_id',
+        'description',
+        'price',
+        'photo',
+        'category',
+    ];
 }
