a37d61c3ae7455fdfd781adc0937422bdd8e0525
SemiRaden <radenbacompanado@su.edu.ph>
2023-10-25 02:16:49 +0800

    hi cher

--- resources/views/customer/custhome.blade.php
+++ resources/views/customer/custhome.blade.php
@@ -9,9 +9,73 @@
 </head>
 <body>
 	<div class="topbar"></div>
-	<div class="content">
-		<img src="{{ asset('asset/TT-txt.png') }}">
-		<h1> Application Platform for Service Providers</h1>
-	</div>
+    <div class="content">
+        <div class="slideshow-container">
+            <div class="mySlides">
+                <img src="{{ asset('asset/img1.jpg') }}" alt="Image 1">
+            </div>
+            <div class="mySlides">
+                <img src="{{ asset('asset/img2.jpg') }}" alt="Image 2">
+            </div>
+            <div class="mySlides">
+                <img src="{{ asset('asset/img3.jpg') }}" alt="Image 3">
+            </div>
+            <div class="mySlides">
+                <img src="{{ asset('asset/img4.jpg') }}" alt="Image 4">
+            </div>
+            <div class="mySlides">
+                <img src="{{ asset('asset/img5.jpg') }}" alt="Image 5">
+            </div>
+            <div class="mySlides">
+                <img src="{{ asset('asset/img6.jpg') }}" alt="Image 6">
+            </div>
+            <div class="mySlides">
+                <img src="{{ asset('asset/img7.jpg') }}" alt="Image 7">
+            </div>
+            <div class="mySlides">
+                <img src="{{ asset('asset/img8.jpg') }}" alt="Image 8">
+            </div>
+
+            <div class="centered-box">
+			    <h1>Task for Help</h1>
+			    <p style="letter-spacing: -6px; color: #470047;">-----------------------</p>		
+			        <p>What task would you like us to tackle?</p>
+			        <br>
+			        <div class="dropdown-container">
+			        <select name="task-dropdown" required>
+			        	<option value="">Choose...</option>
+			            <option value="kitchen">Kitchen</option>
+			            <option value="living-room">Living Room</option>
+			            <option value="bedroom">Bedroom</option>
+			            <option value="bathroom">Bathroom</option>
+			            <option value="plumbing">Plumbing</option>
+			            <option value="electricity">Electricity</option>
+			            <option value="yard-lawn">Yard/Lawn</option>
+			            <option value="others">Others</option>
+			        </select>
+			    <button type="button" class="find-button">Search Services</button>
+			    </div>
+			</div>
+        </div>
+    </div>
+
+    <script>
+        let slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+            let i;
+            const slides = document.getElementsByClassName("mySlides");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.opacity = 0;
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {
+                slideIndex = 1;
+            }
+            slides[slideIndex - 1].style.opacity = 1;
+            setTimeout(showSlides, 5000); // Change image every 2 seconds
+        }
+    </script>
 </body>
 </html>
