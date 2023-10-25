0fcf47200e682c596cbd91dd991af6374968758d
SemiRaden <radenbacompanado@su.edu.ph>
2023-10-25 08:12:42 +0800

    prov service delete modal

--- resources/views/provider/providerServices.blade.php
+++ resources/views/provider/providerServices.blade.php
@@ -89,16 +89,14 @@
 						</form>
 				@endif
 
-				
-
 				<form action="{{ route('delete', ['id' => $services->id]) }}" method="get">
-				<a href="#" id="openModalLink">
+				<a href="#" class="openModalLink" data-target="modal{{$services->id}}">
 				<svg id="delbtn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">	
   				<path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clip-rule="evenodd" /><button class="hide" style ="border: 0; width: 5%;" ></button></svg> 
 			</a>
 
 			<!-- Modal for Terms and Conditions -->
-		      <div id="customModal" class="modal">
+		      <div id="modal{{$services->id}}" class="modal">
 		        <div class="modal-content">
 		            <a href="#" class="close" id="closeModal">&times;</a>
 		            <h2>Confirm Deletion of Service</h2>
@@ -115,8 +113,6 @@
 			</div>
 		</div>
 	</div>
-
-
 		@endforeach
 		<!-- end foreach -->
 	
@@ -175,23 +171,27 @@
         categorySelect.on('change', filterServices);
     });
 
-    const modal = document.getElementById("customModal");
-    const openModalLink = document.getElementById("openModalLink");
-    const closeModalBtn = document.getElementById("closeModal");
+    $(document).ready(function () {
 
-    openModalLink.addEventListener("click", () => {
+    $('.openModalLink').on('click', function (e) {
+        e.preventDefault();
+        const targetModalId = $(this).data('target');
+        const modal = document.getElementById(targetModalId);
         modal.style.display = "block";
     });
 
-    closeModalBtn.addEventListener("click", () => {
+    $('.closeModal').on('click', function () {
+        const targetModalId = $(this).data('target');
+        const modal = document.getElementById(targetModalId);
         modal.style.display = "none";
     });
 
     window.addEventListener("click", (event) => {
-        if (event.target === modal) {
-            modal.style.display = "none";
+        if (event.target.classList.contains("modal")) {
+            event.target.style.display = "none";
         }
     });
+});
 </script>
 
 
